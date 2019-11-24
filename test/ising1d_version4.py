8aa1e8ae4a25b6e7d117b7f2c0e26b85664a3211
Icespellcaster <2743445372@qq.com>
2019-11-24 13:38:53 +0800

    Add files via upload

--- test/ising1d_version4.py
+++ test/ising1d_version4.py
@@ -3,7 +3,8 @@
 
 H = -J * sigma_z^i sigma_z^{i+1} - h * sigma_x^i 的基态能
 
-存在问题：自旋分块同向，导致系统陷入局部最小。
+存在问题1：自旋分块同向，导致系统陷入局部最小。
+问题1已解决
 """
 
 import os 
@@ -19,18 +20,30 @@ tf.set_random_seed(1)
 
 L = 10 # The length of chain
 CHANNEL = 2
-ACTIONS = ['up', 'down']
+# ACTIONS = ['up', 'down']
+# [000, 001, 010, 011, 100, 101, 110, 111]
+# [0,1,2,3,4,5,6,7]
+# ACTIONS = ['ddd', 'ddu', 'dud', 'duu', 'udd', 'udu', 'uud', 'uuu']
+ACTIONS = 8
 MAX_EPISODES = 130
-FRESH_TIME = 0.3
-ITER = 0
+FRESH_TIME = 0.1
 LR = 1e-2
+EPSILON = 0.9
+ALPHA = 0.1
+
+
+def build_q_table(n_states, actions):
+    table = pd.DataFrame(
+        np.zeros((n_states, actions)),
+    )
+
+    return table
 
 
 class TFI_1d:
-    def __init__(self, L, channel, iter, learning_rate=1e-3, momentum=0.9, h=1):
+    def __init__(self, L, channel, learning_rate=1e-3, momentum=0.9, h=1):
         self.L = L
         self.channel = channel
-        self.iter = iter
         self.h = h
         self.init_config()
         self.x = tf.placeholder(tf.float32, [self.L, self.channel])
@@ -60,6 +73,21 @@ class TFI_1d:
         self.config[:,0] = x
         self.config[:,1] = 1 - x
         self.config.astype(np.float32)
+
+    def A2list(self, A):
+        A_list = []
+        A_list.append(A//2//2)
+        A_list.append((A-4*A_list[0])//2)
+        A_list.append((A-4*A_list[0]-2*A_list[1]))
+        return A_list
+
+    def update_config(self, S, A_list):
+        self.config[S, 0] = A_list[0]
+        self.config[S, 1] = 1 - A_list[0]
+        self.config[(S+1) % self.L, 0] = A_list[1]
+        self.config[(S+1) % self.L, 1] = 1 - A_list[1]
+        self.config[(S+2) % self.L, 0] = A_list[2]
+        self.config[(S+2) % self.L, 1] = 1 - A_list[2]
 
     def local_E_Ising(self):
         """
@@ -99,6 +127,7 @@ class TFI_1d:
                 env_list += ['↓']
         return env_list
 
+    '''
     def choose_action(self, state):
         # This is how to choose an action
         
@@ -108,41 +137,32 @@ class TFI_1d:
             action_name = ACTIONS[0] # up
         
         return action_name
+    '''
+
+    def choose_action(self, state, q_table):
+        state_actions = q_table.iloc[0, :]
+        if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):
+            action_name = np.random.choice(ACTIONS)
+        else:
+            action_name = state_actions.idxmax()
+        return action_name
 
     def get_env_feedback(self, S, A, Loss):
         S_ = ((S + 1) % self.L)
         oldlocalE = self.sess.run(self.localE, feed_dict={self.x: self.config})
-        if A == 'up':
-            self.config[S, 0] = 1.
-            self.config[S, 1] = 0.
-            localE = self.sess.run(self.localE, feed_dict={self.x: self.config})
-            if localE < oldlocalE:
-                pass
-            # elif tf.abs(localE - oldlocalE) < 1e-3:
-            #     if self.iter % 2 == 1:
-            #         self.config[S, 0] = 0
-            #         self.config[S, 1] = 1
-            #         localE = oldlocalE
-            #     self.iter += 1
-            else:
-                localE = oldlocalE
-                self.config[S, 0] = 0
-                self.config[S, 1] = 1
-                self.sess.run(self.train_op, feed_dict={self.x: self.config})
-
+        oldconfig = self.config.copy()
+        A_list = self.A2list(A)
+        self.update_config(S, A_list)
+        localE = self.sess.run(self.localE, feed_dict={self.x: self.config})
+        if localE < oldlocalE:
+            R = 2
         else:
-            self.config[S, 0] = 0
-            self.config[S, 1] = 1
-            localE = self.sess.run(self.localE, feed_dict={self.x: self.config})
-            if localE < oldlocalE:
-                pass
-            else:
-                localE = oldlocalE
-                self.config[S, 0] = 1
-                self.config[S, 1] = 0
-                self.sess.run(self.train_op, feed_dict={self.x: self.config})
+            R = -1
+            localE = oldlocalE
+            self.config = oldconfig
+            self.sess.run(self.train_op, feed_dict={self.x: self.config})
         Loss.append(self.sess.run(self.loss, feed_dict={self.x: self.config}))
-        return S_, Loss, localE
+        return S_, Loss, localE, R
 
     def update_env(self, episode):
         # This is how environment be updated
@@ -184,18 +204,25 @@ class TFI_1d:
 
 
 def main():
-    ising = TFI_1d(L=L, channel=CHANNEL, iter=ITER, learning_rate=LR)
+    ising = TFI_1d(L=L, channel=CHANNEL, learning_rate=LR)
     
-    # localE = ising.local_E_Ising(ising.config)
-    # print('init localE: ', localE)
+    q_table = build_q_table(1, ACTIONS)
+    
+    
     # main part of RL loop
     init_env_list = ising.init_env()
     print(''.join(init_env_list))
     S = 0
     Loss = []
     for episode in range(MAX_EPISODES):
-        A = ising.choose_action(S)
-        S_, Loss, localE = ising.get_env_feedback(S, A, Loss)
+        A = ising.choose_action(S, q_table)
+        print('ACTION: ', A)
+        print(q_table)
+        S_, Loss, localE, R = ising.get_env_feedback(S, A, Loss)
+        
+        q_target = R
+        q_table.iloc[0, A] += ALPHA * q_target 
+        
         S = S_
         print('##########################')
         print('episode: %d' % episode)
@@ -203,9 +230,13 @@ def main():
         print()
         print('localE: ', localE/L)
         print('loss: ', Loss[-1])
-        # if episode %10 == 0:
-        #     input()
-        input()
+        if episode %10 == 0:
+            input()
+        # input()
+
 
 if __name__ == '__main__':
     main()
+
+
+
